315b233f4df5d8a02c8d7c40c803cb5b62d42330
Garrett Hyde <ghyde@redhat.com>
2017-07-27 15:47:47 -0700

    Use timestamp tags to backup images

--- webex.sh
+++ webex.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 docker rm --force webex >/dev/null 2>&1
-docker build --tag=webex .
+docker build --tag=webex:latest .
+
+# Sometimes the Webex client isn't compatable with newer versions of Firefox.
+# Use timestamp tags to backup images.
+docker tag webex:latest webex:$(date +"%Y%m%d")
 
 ./run-webex.sh
